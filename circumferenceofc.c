03c309d2821d781b2c14e86fa4ef25fe74381465
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 16:01:59 +0530

    circumference of circle

---
+++ circumferenceofc.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+  int radius, circumference;
+  printf("RA2211042010037\n");
+  printf("Radius: ");
+  scanf("%d", &radius);
+  
+  circumference = 2*3.14*radius;
+  printf("%d", circumference);
+}
