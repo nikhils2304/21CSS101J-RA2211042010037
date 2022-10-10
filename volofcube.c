a1e5871bef8d88cc3134bcc027cd133eb0df0bcc
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 21:58:52 +0530

    volume of cube

---
+++ volofcube.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("RA2211042010037\n");
+  int side, Volume;
+  printf("Side= ");
+  scanf("%d", &side);
+  
+  Volume = side* side*side;
+  printf("Volume: %d", Volume);
+  
+}
+
