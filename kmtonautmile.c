e859d4838dd9ca58463434df6661ba3229e65692
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 22:00:40 +0530

    kilometer to nautical mile

---
+++ kmtonautmile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("RA2211042010037\n");
+  float mile, kilometer;
+  printf("mile= ");
+  scanf("%f", &mile);
+
+  printf("kilometer= %f", kilometer= mile/1.852);
+}
