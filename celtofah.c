c8ad9158c0fd41851928a71f4c818aa61989399e
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 15:57:07 +0530

    celsius to fahrenheit

---
+++ celtofah.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  float farenheit, celcius;
+  printf("RA2211042010037\n");
+  printf("Enter the temperature in celcius: ");
+  scanf("%f", &celcius);
+  farenheit=((celcius*1.8)+32;
+  printf("temperature is :%f", farenheit);
+  return 0;
+}
+
