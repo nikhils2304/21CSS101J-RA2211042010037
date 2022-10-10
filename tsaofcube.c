77beb84bdba2d30381b1b7d78a6f7a736dabbf29
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 22:30:52 +0530

    tsa of cube

---
+++ tsaofcube.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("RA2211042010037\n");
+  int side, TSA;
+  printf("Side= ");
+  scanf("%d", &side);
+  
+  TSA = 6* side*side;
+  printf("TSA: %d", TSA);
+  
+}
