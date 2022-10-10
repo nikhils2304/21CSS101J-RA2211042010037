05e71cc52f7811931b4add48007b9f24136be7dc
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 15:38:52 +0530

    area of circle

---
+++ areaofcircle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int radius, area;
+  printf("Radius of circle: ");
+  scanf("%d", &radius);
+  
+  area = 3.14*radius*radius;
+  printf("Area of circle= %d", area);
+  
+}
+
