53f2f2c66063adbaef56c3a83aa312ef86d0c82e
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 15:47:35 +0530

    area of equilateral triangle

---
+++ areaofequitri.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int area, side;
+  printf("RA2211042010037\n");
+  printf("Side= ");
+  scanf("%d", &side);
+  
+  area= 0.433*side*side;
+  printf("%d", area);
+}
