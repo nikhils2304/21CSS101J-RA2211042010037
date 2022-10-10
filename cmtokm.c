302850f472c47b48db9fadfc1438a432b06bcb47
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 15:59:47 +0530

    centimeter to kilometer

---
+++ cmtokm.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  float centi, kilo;
+  printf("RA2211042010037\n");
+  printf("Centimeter: ");
+  scanf("%f", &centi);
+
+  printf("Kilometer:%f", centi= kilo/100000);
+}
