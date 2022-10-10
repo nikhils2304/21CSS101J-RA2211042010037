aac2d1ad4f9f2d5e65597239242a8a734d376f9f
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 15:41:45 +0530

    division

---
+++ div.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  float a,b;
+  printf("RA2211042010037\n");
+  printf("Please enter the numbers you want to multiply\n");
+  scanf("%f%f", &a,&b);
+  printf("%f", a/b);
+}
