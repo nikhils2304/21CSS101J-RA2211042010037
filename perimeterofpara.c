8b5ad7e5d5ce46d80c467feb406a7184f794ae98
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 22:11:43 +0530

    perimeter of parallelogram

---
+++ perimeterofpara.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  float perimeter, length, base;
+  printf("Base: ");
+  scanf("%f", &base);
+  printf("Length:");
+  scanf("%f", &length);
+         
+  printf("%f", perimeter = 2*(length*base));
+}
+Footer
