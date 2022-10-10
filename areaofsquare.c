d54476dddf32e7b3d6fd2cd16c7f70ffad2e4d64
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 15:50:12 +0530

    area of square

---
+++ areaofsquare.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  int side, area;
+  print("RA2211042010037\n");
+  printf("Side of square= ");
+  scanf("%d", &side);
+  
+  area = side*side;
+  printf("Area of Square= %d", area);
+  
+}
