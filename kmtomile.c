62076388c7e5e8883272ec00844c57932fd88b6d
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 19:15:17 +0530

    kilometer to mile

---
+++ kmtomile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("RA2211042010037\n");
+  float mile, kilometer;
+  printf("Kilometer= ");
+  scanf("%f", &kilometer);
+  
+  printf("mile= %f", mile= kilometer*1.609);
+}
