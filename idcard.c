f20f75c318db95765d7b49d95987825a63725eab
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 19:04:06 +0530

    identity card

---
+++ idcard.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  // Name of Institute
+  printf("SRM institute of Science and Technology\n");
+  // Name of Student 
+  printf("Nikhil Shinde\n");
+  // Course
+  printf("B.Tech in Computer Science and Buissness Systems\n");
+  // Regsitration Number
+  printf("RA2211042010037\n");
+  
+  return 0;
+}
