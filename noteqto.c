869db76d4431cd9fff2cd2ee8dcaeb7402414b6e
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 19:27:59 +0530

    to show that two nos. are unequal

---
+++ noteqto.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf("RA2211042010037\n");
+  int x = 12;
+  int y = 4;
+  printf("%d", x != y); // returns 1 (true) because 12 is not equal to 4
+  return 0;
+}
