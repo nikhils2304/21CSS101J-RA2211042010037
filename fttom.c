15d87e0acce156e953d566c670c95cb958f79c10
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-10 19:13:27 +0530

    foot to meter

---
+++ fttom.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+  {
+  printf("RA2211042010037\n");
+  float foot, meter;
+  printf("Foot: ");
+  scanf("%f", &foot);
+  
+  printf("Meter=%f", meter= foot/ 3.281);
+  }
