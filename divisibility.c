de3fb64758fcf14174bedd2a02a8e4ca03d5a34d
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-11 20:38:05 +0530

    checking divisibility

---
+++ divisibility.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010037\n");
+    printf("enter number");
+    int a;
+    scanf("%d",&a);
+    if(a%5==0)
+    {
+        printf("divisible by 5");
+    }
+    else
+    {
+        printf("not divisible by 5");
+    }
+}
