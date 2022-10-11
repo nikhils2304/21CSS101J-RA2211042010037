9d860b794df0f08d1d2b2c903f05f51ac4727300
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-11 14:06:43 +0530

    tsa of right circular cone

---
+++ tsarcco.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("RA2211042010037\n");
+  float radius, length, TSA;
+  printf("Radius: ");
+  scanf("%f", &radius);
+  printf("length: ");
+  scanf("%f", &length);
+  
+  TSA = radius*3.14*(length+radius);
+  printf("%f", TSA);
+}
