f4ad9d0d317506f42c71b1e6c4223fb76be9f642
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-11 20:44:22 +0530

    pass or fail

---
+++ passfail.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010037/n");
+    printf("enter marks of maths, science,english");
+    int m,s,e;
+    scanf("%d %d %d",&m,&s,&e);
+    int sum=m+s+e;
+    float p=(sum/300.0)*100;
+    if(p>40)
+    {
+        printf("pass");
+    }
+    else
+    {
+        
+        printf("fail");
+    }
+}
