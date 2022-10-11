175b126729e9a90fe4bf5f43cb96c22fee7726b3
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-11 13:51:12 +0530

    tsa of right circular cylinder

---
+++ tsarccy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("RA2211042010037\n");
+  int radius, height, TSA;
+  
+  printf("Radius= ");
+  scanf("%d", &radius);
+  printf("Height= ");
+  scanf("%d", &height);
+  
+  TSA = 3.14*radius*radius*height;
+  printf("%d", TSA);
+  
+}
