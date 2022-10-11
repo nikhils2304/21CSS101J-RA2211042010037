370d7157541cd6584570260b41a754dffba77dd8
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-11 20:40:19 +0530

    no..

---
+++ greaterthan100.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010037\n");
+    printf("enter number");
+    int a;
+    scanf("%d",&a);
+    if(a>100)
+    {
+        printf("greater than 100");
+    }
+    else
+    {
+        printf("less than 100");
+    }
+}
