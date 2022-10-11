5c67ddb0f20cdef3929c0024bcac5459ed0ee230
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-11 20:35:43 +0530

    checking if adult or not

---
+++ adultornot.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010037\n");
+    printf("enter age");
+    int a;
+    scanf("%d",&a);
+    if(a>18)
+    {
+        printf(" adult");
+    }
+    else
+    {
+        printf(" not adult");
+    }
+}
