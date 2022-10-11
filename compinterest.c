9435fc7787c73b9fa01dc603cce48f3f452faca4
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-11 20:46:21 +0530

    compound interest

---
+++ compinterest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+main()
+{
+    printf("RA2211042010037\n");
+    float pa/*principal amount*/,t/*time*/;
+    float r,ci,si;
+    printf("enter a principal,rate,time\n");
+    scanf("%f %f %f",&pa,&r,&t);
+    ci=(pa*(pow((1+r/(100))),t)-pa;
+    printf("compound interest=%f",ci);
+}
