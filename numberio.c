3d18fa3466ae3a3b51ca87a28dd543a572f82ceb
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-18 09:10:45 +0530

    Update numberio.c

--- numberio.c
+++ numberio.c
@@ -4,10 +4,8 @@ int main() {
     printf("RA2211042010037\n");
     printf("Enter an integer: ");  
     
-    // for reading and storing input
     scanf("%d", &number);
-
-    // for displaying output
+    
     printf("You entered: %d", number);
     
     return 0;
