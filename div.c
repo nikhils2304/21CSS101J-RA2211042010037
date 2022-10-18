9847942ecc5f0c3be0ab758885d9e7c3a03578eb
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-18 08:58:37 +0530

    division

--- div.c
+++ div.c
@@ -4,7 +4,7 @@ int main()
 {
   float a,b;
   printf("RA2211042010037\n");
-  printf("Please enter the numbers you want to multiply\n");
+  printf("Please enter the numbers you want to divide\n");
   scanf("%f%f", &a,&b);
   printf("%f", a/b);
 }
