03bfcdd8532a13b0b7ab300d9b758e7c1a20d554
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-18 08:55:00 +0530

    area of triangle

--- areaoftriangle.c
+++ areaoftriangle.c
@@ -4,9 +4,9 @@ int main()
 {
   int height, base, area;
   printf("RA2211042010037\n");
-  printf("Enter the height of the triangle:");
+  printf("Height of the triangle:");
   scanf("%d", &height);
-  printf("Enter the base of the triangle:");
+  printf("Base of the triangle:");
   scanf("%d", &base);
   
   area = 0.5* height * base;
