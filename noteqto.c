939b40b973f15843f2c47ed162e4b001f48950ec
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-10-18 09:10:04 +0530

    not equal to

--- noteqto.c
+++ noteqto.c
@@ -4,6 +4,6 @@ int main() {
   printf("RA2211042010037\n");
   int x = 12;
   int y = 4;
-  printf("%d", x != y); // returns 1 (true) because 12 is not equal to 4
+  printf("%d", x != y); 
   return 0;
 }
