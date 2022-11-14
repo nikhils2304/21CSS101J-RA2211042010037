6cb5f0667359f9ac4fdb0978a1dd3e7eb3ba5fb7
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-11-14 09:12:11 +0530

    odd/even using ternary

---
+++ cdttr3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  
+  int n;
+  printf("RA2211042010037\n");
+  printf("n : ");
+  scanf("%d", &n);
+  (n % 2 == 0) ? printf("%d is even", n) : printf("%d is odd", n);
+  
+  return 0;
+  
+}
+  
