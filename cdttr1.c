c0312b01695d54cf9c113e4e736eee7c9cded50b
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-11-14 08:51:04 +0530

    arithmetic operations

---
+++ cdttr1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  
+  int a = 20, b = 3;
+  printf("RA2211042010037\n");
+  printf("Addition Result = %d\n", a + b);
+  printf("Subtraction Result = %d\n", a - b);
+  printf("Multiplication Result = %d\n", a * b);
+  printf("Division Result = %d\n", a / b);
+  printf("Remainder Result = %d\n", a % b);
+  
+  return 0;
+  
+}
