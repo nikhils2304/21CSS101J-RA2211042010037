b4094a9e08af171e7122cad42f2b2fc8d0225676
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-11-14 09:00:44 +0530

    ternary operator

---
+++ cdttr2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+  
+  int num1, num2, largest;
+  printf("RA2211042010037\n");
+  printf("enter the two nos. : ");
+  scanf("%d %d", &num1, &num2);
+  largest = num1 > num2 ? num1 : num2;
+  printf("the larger no. is : %d\n", largest);
+  return 0;
+  
+}
+  
