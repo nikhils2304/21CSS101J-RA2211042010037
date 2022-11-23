cbdc5fba2b83e3e2f7e037c69b8c6e6718aa977e
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-11-23 21:51:41 +0530

    largest element in array

---
+++ largestinarray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    int mark[20],i,j,temp;
+    printf("enter the 20 values to input in array!\n");
+    for(i=0;i<20;i++)
+        {
+            scanf("%d",&mark[i]);
+        }
+        for(j=0;j<19;j++)
+        {
+            if(mark[j]>mark[j+1])
+            {
+                temp=mark[j];
+                mark[j]=mark[j+1];
+                mark[j+1]=temp;
+            }
+        }
+        printf("max is :%d",mark[19]);
+} 
