205df0c15a592d5ded6b5fbde40b3de56159c26a
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-11-24 21:00:24 +0530

    using gets()

---
+++ usinggets.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ 
+  printf("RA2211042010037\n");
+  printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+  char str[50]; 
+  printf("Enter your complete name : \n\n\n");
+  gets(str);
+  printf("\n\nWelcome to studytonight %s\n\n\n" , str);
+  printf("\n\n\t\tCoding is Fun !\n\n\n");
+  return 0;
+}
+
