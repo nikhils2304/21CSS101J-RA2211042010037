283a3ba6014c0637ee3d72e67dc78bc04e706486
nikhils2304 <113490494+nikhils2304@users.noreply.github.com>
2022-11-24 21:34:47 +0530

    problem based

---
+++ cdttr15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Register number: RA2211042010037\n");
+    float a,b,c;
+    scanf("%f %f",&a,&b);
+    c = a*b;
+    printf("%0.4f",c);
+
+    return 0;
+}
